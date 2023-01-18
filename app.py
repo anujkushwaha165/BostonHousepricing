53dfca6aff947e1fb498226d003bd923d85dcaad
Anuj Kushwaha <anujkushwaha165@gmail.com>
2023-01-18 16:11:11 +0530

    the web app front end

--- app.py
+++ app.py
@@ -24,16 +24,17 @@ def predict_api():
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
-    # return print('Anuj')
-# @app.route('/predict_api',methods=['POST'])
-# def predict_api():
-#     data=request.json['data']
-#     print(data)
-#     print(np.array(list(data.values())).reshape(1,-1))
-#     new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
-#     output=regmodel.predict(new_data)
-#     print(output[0])
-#     return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)
+    print(output[0])
+    return render_template("home.html",prediction_text="the House price pridiction is{}".format(output))
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
